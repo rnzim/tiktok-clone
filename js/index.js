8abb76795b78f30824561fda2506233a584ddc5d
Raimundo Nonato <144294493+rnzim@users.noreply.github.com>
2023-11-26 07:41:43 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,6 +10,6 @@ btn_next.addEventListener('click',()=>{
     })
     
     var video = document.querySelector('.vdo')
-    video.src = 'https://firebasestorage.googleapis.com/v0/b/storagex-5974a.appspot.com/o/lv_0_20231126005727.mp4?alt=media&token=9b8f90c0-c7f0-4a21-9e3c-ccbebb2fd25f'
+    video.src = `${vdos.data.url[parseInt(Math.random()*vdos.data.url.length)]}`
     console.log('Url Do Video: '+video.src)
 })
